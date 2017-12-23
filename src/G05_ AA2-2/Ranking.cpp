c7f65678f09ac2da5e1fb376dfa51f545c218a6b
danielom98 <danielom98@gmail.com>
2017-12-23 12:42:54 +0100

    casi casi de los casis

--- src/G05_ AA2-2/Ranking.cpp
+++ src/G05_ AA2-2/Ranking.cpp
@@ -31,96 +31,107 @@ Ranking::Ranking()
 	color = { 0, 255, 255, 0 };
 	scores.color = color;
 	scores.text = "works?";
-	scores.h = 50;
+	scores.h = 30;
 	scores.w = 200;
 	scoresXpositionText = SCREEN_WIDTH / 2 - scores.w / 2;
-	scoresYpositionText = 60;
+	scoresYpositionText = 120 + 30;
 	Renderer::Instance()->LoadFont(font);
 	scores_Rect = { scoresXpositionText, scoresYpositionText, scores.w, scores.h };
+	scores.id = "0";
 
 	scores1.color = color;
 	scores1.text = "works?";
-	scores1.h = 50;
+	scores1.h = 30;
 	scores1.w = 200;
 	scores1XpositionText = SCREEN_WIDTH / 2 - scores1.w / 2;
-	scores1YpositionText = 120;
+	scores1YpositionText = 164 + 30;
 	scores1_Rect = { scores1XpositionText, scores1YpositionText, scores1.w, scores1.h };
+	scores1.id = "1";
 
 	scores2.color = color;
 	scores2.text = "works?";
-	scores2.h = 50;
+	scores2.h = 30;
 	scores2.w = 200;
 	scores2XpositionText = SCREEN_WIDTH / 2 - scores2.w / 2;
-	scores2YpositionText = 180;
+	scores2YpositionText = 204 + 30;
 	scores2_Rect = { scores2XpositionText, scores2YpositionText, scores2.w, scores2.h };
+	scores2.id = "2";
 
 	scores3.color = color;
 	scores3.text = "works?";
-	scores3.h = 50;
+	scores3.h = 30;
 	scores3.w = 200;
 	scores3XpositionText = SCREEN_WIDTH / 2 - scores3.w / 2;
-	scores3YpositionText = 240;
+	scores3YpositionText = 244 + 30;
 	scores3_Rect = { scores3XpositionText, scores3YpositionText, scores3.w, scores3.h };
+	scores3.id = "3";
+
 
 	scores4.color = color;
 	scores4.text = "works?";
-	scores4.h = 50;
+	scores4.h = 30;
 	scores4.w = 200;
 	scores4XpositionText = SCREEN_WIDTH / 2 - scores4.w / 2;
-	scores4YpositionText = 300;
+	scores4YpositionText = 284 + 30;
 	scores4_Rect = { scores4XpositionText, scores4YpositionText, scores4.w, scores4.h };
+	scores4.id = "4";
 
 	scores5.color = color;
 	scores5.text = "works?";
-	scores5.h = 50;
+	scores5.h = 30;
 	scores5.w = 200;
 	scores5XpositionText = SCREEN_WIDTH / 2 - scores5.w / 2;
-	scores5YpositionText = 360;
+	scores5YpositionText = 324 + 30;
 	scores5_Rect = { scores5XpositionText, scores5YpositionText, scores5.w, scores5.h };
+	scores5.id = "5";
 
 	scores6.color = color;
 	scores6.text = "works?";
-	scores6.h = 50;
+	scores6.h = 30;
 	scores6.w = 200;
 	scores6XpositionText = SCREEN_WIDTH / 2 - scores6.w / 2;
-	scores6YpositionText = 420;
+	scores6YpositionText = 364 + 30;
 	scores6_Rect = { scores6XpositionText, scores6YpositionText, scores6.w, scores6.h };
+	scores6.id = "6";
 
 	scores7.color = color;
 	scores7.text = "works?";
-	scores7.h = 50;
+	scores7.h = 30;
 	scores7.w = 200;
 	scores7XpositionText = SCREEN_WIDTH / 2 - scores7.w / 2;
-	scores7YpositionText = 480;
+	scores7YpositionText = 404 + 30;
 	scores7_Rect = { scores7XpositionText, scores7YpositionText, scores7.w, scores7.h };
+	scores7.id = "7";
 
 	scores8.color = color;
 	scores8.text = "works?";
-	scores8.h = 50;
+	scores8.h = 30;
 	scores8.w = 200;
 	scores8XpositionText = SCREEN_WIDTH / 2 - scores8.w / 2;
-	scores8YpositionText = 540;
+	scores8YpositionText = 444 + 30;
 	scores8_Rect = { scores8XpositionText, scores8YpositionText, scores8.w, scores8.h };
+	scores8.id = "8";
 
 	scores9.color = color;
 	scores9.text = "works?";
-	scores9.h = 50;
+	scores9.h = 30;
 	scores9.w = 200;
 	scores9XpositionText = SCREEN_WIDTH / 2 - scores9.w / 2;
-	scores9YpositionText = 600;
+	scores9YpositionText = 484 + 30;
 	scores9_Rect = { scores9XpositionText, scores9YpositionText, scores9.w, scores9.h };
+	scores9.id = "9";
 
 	std::vector<std::pair<int, std::string>> ranking;
 	ranking.push_back({ -1," " });
 	ranking.push_back({ -1," " });
-	ranking.push_back({ -1," " });
-	ranking.push_back({ -1," " });
-	ranking.push_back({ -1," " });
-	ranking.push_back({ -1," " });
-	ranking.push_back({ -1," " });
-	ranking.push_back({ -1," " });
-	ranking.push_back({ -1," " });
-	ranking.push_back({ -1," " });
+	ranking.push_back({ -1 ," " });
+	ranking.push_back({ -1 ," " });
+	ranking.push_back({ -1 ," " });
+	ranking.push_back({ -1 ," " });
+	ranking.push_back({ -1 ," " });
+	ranking.push_back({ -1 ," " });
+	ranking.push_back({ -1 ," " });
+	ranking.push_back({ -1 ," " });
 	std::string playerName;
 	int score;
 	std::ifstream fentrada("../../res/files/Ranking.bin", std::ios::binary);
@@ -134,22 +145,18 @@ Ranking::Ranking()
 	std::sort(ranking.rbegin(), ranking.rend());
 
 	for (const auto &p : ranking) {
-		std::sort(ranking.rbegin(), ranking.rend());
+		std::cout << p.first << p.second << std::endl;
 	}
-		scores.text = ranking[0].second + ' ' + std::to_string(ranking[0].first);
-		scores1.text = ranking[1].second + ' ' + std::to_string(ranking[1].first);
-		scores2.text = ranking[2].second + ' ' + std::to_string(ranking[2].first);
-		scores3.text = ranking[3].second + ' ' + std::to_string(ranking[3].first);
-		scores4.text = ranking[4].second + ' ' + std::to_string(ranking[4].first);
-		scores5.text = ranking[5].second + ' ' + std::to_string(ranking[5].first);
-		scores6.text = ranking[6].second + ' ' + std::to_string(ranking[6].first);
-		scores7.text = ranking[7].second + ' ' + std::to_string(ranking[7].first);
-		scores8.text = ranking[8].second + ' ' + std::to_string(ranking[8].first);
-		scores9.text = ranking[9].second + ' ' + std::to_string(ranking[9].first);
-	
-	//scores1.text = std::to_string(ranking[1].first) + ranking[1].second;
-
-
+	scores.text = ranking[0].second + ' ' + std::to_string(ranking[0].first);
+	scores1.text = ranking[1].second + ' ' + std::to_string(ranking[1].first);
+	scores2.text = ranking[2].second + ' ' + std::to_string(ranking[2].first);
+	scores3.text = ranking[3].second + ' ' + std::to_string(ranking[3].first);
+	scores4.text = ranking[4].second + ' ' + std::to_string(ranking[4].first);
+	scores5.text = ranking[5].second + ' ' + std::to_string(ranking[5].first);
+	scores6.text = ranking[6].second + ' ' + std::to_string(ranking[6].first);
+	scores7.text = ranking[7].second + ' ' + std::to_string(ranking[7].first);
+	scores8.text = ranking[8].second + ' ' + std::to_string(ranking[8].first);
+	scores9.text = ranking[9].second + ' ' + std::to_string(ranking[9].first);
 }
 
 
